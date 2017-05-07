ca5b62054377f3e2e1b1ac76aaff4b4775b43d73
LAMMINAHO\Petri <lammpe77@gmail.com>
2017-05-08 01:02:12 +0300

    parser toimii

--- feedParser.py
+++ feedParser.py
@@ -1,4 +1,12 @@
 import feedparser
+import bs4
+from urllib.request import urlopen as uReq
 
-print('testi')
+feeds = feedparser.parse('http://feeds.yle.fi/uutiset/v1/recent.rss?publisherIds=YLE_UUTISET&concepts=18-34837')
+print( feeds)
+
+for post in feeds.entries:
+
+    print(post.title + ': '+ post.link + "\n")
+
 
