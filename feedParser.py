19f61fdc88029ad0aeca32d2b7bac1ea95f9384b
LAMMINAHO\Petri <lammpe77@gmail.com>
2017-05-09 01:48:31 +0300

    Newspaper Mukaan projektiin toimii jotenkuten

--- feedParser.py
+++ feedParser.py
@@ -1,12 +1,30 @@
 import feedparser
+from newspaper import Article
+
 import bs4
 from urllib.request import urlopen as uReq
 
-feeds = feedparser.parse('http://feeds.yle.fi/uutiset/v1/recent.rss?publisherIds=YLE_UUTISET&concepts=18-34837')
-print( feeds)
 
-for post in feeds.entries:
 
-    print(post.title + ': '+ post.link + "\n")
+def parseFeed():
+    feeds = feedparser.parse('http://feeds.yle.fi/uutiset/v1/recent.rss?publisherIds=YLE_UUTISET&concepts=18-34837')
+    for post in feeds.entries:
+      print("")
+      # print(post.title + ': ' + post.link + "\n")
 
+    news = feeds.entries[0]
+    print(news.link)
+    parseArticle(news.link)
+
+def parseArticle(url):
+    article = Article(url)
+    article.download()
+    article.parse()
+    #print (article.html)
+    print(article.title)
+    print(article.text)
+     
+
+
+parseFeed() #Toimii
 
