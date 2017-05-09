0f25bd60c668e167f03641a338d4627fdef97ab4
LAMMINAHO\Petri <lammpe77@gmail.com>
2017-05-09 14:55:50 +0300

    Pieniä muutoksia

--- feedParser.py
+++ feedParser.py
@@ -1,30 +1,39 @@
 import feedparser
+import bs4
 from newspaper import Article
 
 import bs4
 from urllib.request import urlopen as uReq
-
-
+#http://feeds.yle.fi/uutiset/v1/recent.rss?publisherIds=YLE_UUTISET
+authors = ""
+title = ""
+date = ""
 
 def parseFeed():
-    feeds = feedparser.parse('http://feeds.yle.fi/uutiset/v1/recent.rss?publisherIds=YLE_UUTISET&concepts=18-34837')
+    feeds = feedparser.parse('http://feeds.yle.fi/uutiset/v1/recent.rss?publisherIds=YLE_UUTISET')
     for post in feeds.entries:
       print("")
-      # print(post.title + ': ' + post.link + "\n")
+       #print(post.title + ': ' + post.link + "\n")
 
     news = feeds.entries[0]
     print(news.link)
     parseArticle(news.link)
 
 def parseArticle(url):
+    global authors
+    global date
+    global title
     article = Article(url)
     article.download()
     article.parse()
-    #print (article.html)
-    print(article.title)
-    print(article.text)
+    authors = article.authors
+    title =  article.title
+    date = article.publish_date
+   # print(article.text)
 
 
 
 parseFeed() #Toimii
 
+print(title)
+print(authors)
