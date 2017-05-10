2dbd4ef0467c735b9fe1669995155dfdf42ef6b5
LAMMINAHO\Petri <lammpe77@gmail.com>
2017-05-11 02:24:08 +0300

    Artikelin parsiminen onnistuu

--- feedParser.py
+++ feedParser.py
@@ -1,19 +1,20 @@
 import feedparser
 import bs4 as bs
 from newspaper import Article
+#from urllib.request import urlopen as uReq
 
-import bs4
-from urllib.request import urlopen as uReq
-#http://feeds.yle.fi/uutiset/v1/recent.rss?publisherIds=YLE_UUTISET
+MTV3_ARTICLE_CLASS = 'editorial'
+YLE_ARTICLE_CLASS = 'yle__article__content'
 authors = ""
 title = ""
 date = ""
 
 def parseFeed():
-    feeds = feedparser.parse('http://feeds.yle.fi/uutiset/v1/recent.rss?publisherIds=YLE_UUTISET')
+    #feeds = feedparser.parse('http://feeds.yle.fi/uutiset/v1/recent.rss?publisherIds=YLE_UUTISET')
+    feeds = feedparser.parse('http://www.mtv.fi/api/feed/rss/uutiset_uusimmat')
     for post in feeds.entries:
       print("")
-       #print(post.title + ': ' + post.link + "\n")
+      # print(post.title + ': ' + post.link + "\n")
 
     news = feeds.entries[0]
     print(news.link)
@@ -31,19 +32,48 @@ def parseArticle(url):
     authors = article.authors
     title = article.title
     date = article.publish_date
-    #print(article.text)
+
+
+   # print(article.text)
     #print(article.summary)
    # len(article.summary)
     make_soup(sauce)
 
 def make_soup(sauce):
     soup = bs.BeautifulSoup(sauce, 'lxml')
+    parseMTV3(soup)
     #print(soup.find_all('p'))
    # print(soup.title.text)
 
-    for para in soup.find_all('p'):
-        print(para.text + '\n')
+
+
 #    print(soup.get_text())
+
+def parseArticle(soup, CLASS_NAME):
+    article = soup.findAll("div", {"class": CLASS_NAME})
+    for para in article:
+        print(para.text)
+
+
+
+
+
+def parseMTV3(soup):
+    article = soup.findAll("div", {"class": MTV3_ARTICLE_CLASS})
+    for para in article:
+       #print(para.text)
+       print(para.text)
+
+def parseYLE(soup):
+    article = soup.findAll("div", {"class": YLE_ARTICLE_CLASS})
+   # print(article)
+    for para in article:
+        print(para.text)
+
+   # for para in soup.find_all('p'):
+    #    print(para.text + '\n')
+
+
 
 
 parseFeed()
