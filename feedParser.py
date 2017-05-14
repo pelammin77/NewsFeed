b1bff91872d0b5fdea8be65358002ce4f81cf835
LAMMINAHO\Petri <lammpe77@gmail.com>
2017-05-15 01:17:59 +0300

    Summary luokka aloitettu Uutisen parsimista parannettu

--- feedParser.py
+++ feedParser.py
@@ -1,9 +1,35 @@
+from nltk.tokenize import sent_tokenize,word_tokenize
+from nltk.corpus import stopwords
+from collections import defaultdict
+from string import punctuation
+from heapq import nlargest
 import feedparser
 import bs4 as bs
-import nltk, re, pprint
-from nltk import word_tokenize
+#import nltk, re, pprint
+#from nltk import word_tokenize
 from newspaper import Article
 #from urllib.request import urlopen as uReq
+
+class Summarizer:
+    def __init__(self, min_sum=0.1, max_sum=0.9):
+        self._min_sum = min_sum
+        self._max_sum = max_sum
+        self._stopwords = set(stopwords.words('finnish') + list(punctuation))
+
+    def _compute_words(self, words):
+        pass
+
+    def _summarize(self, text, n):
+        pass
+
+    def _rank(self, ranking, n):
+        return nlargest(n,ranking, key=ranking.get)
+
+
+
+
+
+
 
 MTV3_ARTICLE_CLASS = 'editorial'
 YLE_ARTICLE_CLASS = 'yle__article__content'
@@ -82,10 +108,11 @@ def parseYLE(soup):
         print(para.text)
 
 def parseNews(soup,articleClass):
-    article = soup.findAll("div", {"class": articleClass}, 'p')
+    [s.extract() for s in soup('style')]
+    article = soup.findAll("div",  {"class": articleClass}, "p")
     # print(article)
     for para in article:
-        print(para.text)
+        print(para)
 
 
 
